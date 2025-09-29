a7ec2bec8e004246be611287ea852beb1a7cb58f
Lasenda <lasendablog@gmail.com>
2025-09-29 20:17:36 +0200

    añado plugin rss pero demomento no sirve de mucho, solo para el xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,47 @@
-module.exports = function(eleventyConfig) {
-  // Copiar Assets tal cual
-  eleventyConfig.addPassthroughCopy("src/Assets");
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
 
-  // Copiar todos los .html de src directamente (sin que 11ty los procese)
-  eleventyConfig.addPassthroughCopy("src/**/*.html");
+module.exports = function(eleventyConfig) {
+  // Plugin RSS: añade filtros RSS y date para Nunjucks
+  eleventyConfig.addPlugin(rssPlugin);
+
+  // Copiar solo assets (CSS, imágenes, JS)
+  eleventyConfig.addPassthroughCopy("src/Assets/css");
+  eleventyConfig.addPassthroughCopy("src/Assets/img");
+  eleventyConfig.addPassthroughCopy("src/Assets/js");
+
+  // Colección "posts"
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/articulos/**/*.{md,html}")
+      .filter(post => post.data && post.data.title)
+      .sort((a, b) => b.date - a.date);
+  });
+
+  // Filtro "date"
+  eleventyConfig.addFilter("date", (dateObj, format = "yyyy LLL dd") => {
+    return DateTime.fromJSDate(dateObj).toFormat(format);
+  });
+
+  // Permalink global SOLO para páginas HTML/Markdown
+  eleventyConfig.addGlobalData("permalink", () => (data) => {
+    // Si es XML (feed), no tocar (usa frontmatter)
+    if (data.page.inputPath && data.page.inputPath.endsWith(".xml.njk")) {
+      return data.page.filePathStem + data.page.outputFileExtension;
+    }
+    // Para MD, NJK o HTML, forzar .html
+    return `${data.page.filePathStem}.html`;
+  });
 
   return {
     dir: {
       input: "src",
       output: "_site"
     },
-    templateFormats: ["md"], // solo procesar Markdown
-    htmlTemplateEngine: false // no tocar los HTML
+    // Incluir XML para procesar feed.xml.njk
+    templateFormats: ["md", "njk", "html", "xml"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    pathPrefix: ""
   };
 };
